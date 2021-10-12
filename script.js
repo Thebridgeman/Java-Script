afbdf820c64718d07937ce0982f4e3cbb997cf1a
William Bridgeman <will.bridgeman@yahoo.co.uk>
2021-10-12 15:22:08 +0100

    afternoon commit

--- script.js
+++ script.js
@@ -99,8 +99,8 @@ let totalLeftToPay = totalMoney - moneyPaidSoFar;
 console.log(`The total bill is £${totalMoney} the remaining ammount of money to be paid is £${totalLeftToPay}`);
 
 
-let a;
-let b = "12345";
+let x;
+let y = "12345";
 let c = 12344;
 let d = true;
 let e = {a:"JavaScript"};
@@ -108,8 +108,271 @@ let e = {a:"JavaScript"};
 
 
 console.log(
-    typeof(a),
-    typeof(b),
+    typeof(x),
+    typeof(y),
     typeof(c),
     typeof(d),
     typeof(e),);
+
+
+
+// ASI and Iteration 
+
+//automatic semi colon insrtion - can lead to unwanter behavior 
+
+//e.g. this should incrament a by 1 but it incraments b instead
+
+let a = 1
+let b = 1
+
+a
+++
+b
+
+console.log(`a = ${a}, b = ${b}`)
+
+// to fix 
+
+let m = 1;
+let n = 1;
+
+m;
+++n;
+
+console.log(`m = ${m}, n = ${n}`);
+
+
+// Iterating 
+// loops 
+
+// ! FOR loop - use a counter
+
+for(let i = 0; i < 10; i++){
+    console.log(`i = ${i}`);
+}
+
+// while loop 
+
+let condition = true;
+let incrament = 0;
+
+while(condition) {
+    console.log(`incremant me!`)
+    incrament++;
+
+    if(incrament == 3) {
+        condition = false;
+    }
+}
+
+// do while loop
+
+let con = false;
+let inc = 0;
+
+do {
+    console.log(`incremant me!`)
+    inc++;
+
+    if(inc == 3) {
+        con = false;
+    }
+} while(con);
+
+
+//switch cases
+
+let num = 1;
+
+switch(num) {
+    case 0:
+    console.log('We are at zero');
+    break;
+
+    case 1:
+        console.log('number is 1');
+    case 4: 
+    console.log('number is 4');
+    case 4:
+        console.log('at the end');
+        break;
+        default:
+            console.log('this is default');
+}
+
+// ASI QA page task
+
+
+let p = 10;
+let o = 5;
+
+p
+++
+o;
+
+console.log(`p = ${p}, o = ${o}`);
+
+function sayHello(){
+    return;
+    {
+        "someone says hi";
+    }
+
+}
+
+// iteration page QA exercise 1
+
+function loop()
+{
+    for(let a = 100; a < 200; a++){
+        console.log('A');
+    }
+
+}
+loop();
+
+// iteration page QA exercise 2
+
+
+function loop2()
+{
+    for(let a = 100; a <= 200; a++){
+        if(a%2 == 0)
+        {
+            console.log('-');
+        }
+        else
+        {
+            console.log('*');
+        }
+    }
+
+}
+loop2();
+
+// iteration page QA Method to print 1-10 10 times each
+
+function loop3(){
+for (let i = 0; i < 10; i++)
+{
+    for (let j = 1; j <= 10; j++) {
+      console.log(j);
+    }
+ }  
+}
+loop3();
+
+// iteration page QA switch case statement 
+
+let now = new Date(); 
+let day = now.getDay();
+switch (day) {
+  case 0:
+    console.log(`It's Sunday`);
+    break;
+  case 6:
+    console.log(`It's Saturday`);
+    break;
+  case 1:
+    console.log(`It's Monday`);
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    console.log(`It's a weekday`);
+    break;
+  default:
+    console.log(`Excuse me?`);
+    break;
+}
+
+// Conditionals 
+
+//Truthly and Falsly 
+
+// in JS each value has an inherent boolean value 
+
+// Excercise
+
+//console.log(strictA == strictB); 
+//console.log(strictA === strictB);
+
+
+let age = 15;
+
+if (age >= 18 && age <= 65) {
+  console.log("Age is in range");
+} else if (age < 18) {
+  console.log("Underage");
+} else {
+  console.log("Catch all else");
+}
+
+// let age = 100;
+//let result = age >= 50 ? "50 or over" : "Under 50";
+//console.log(result);
+
+// OBJECTS ARRAYS AND JSON
+
+//OBJECTS 
+// An object can be defined as an unordered collection of related data, of primitive or reference types in the form of key:value pairs.
+
+let animal = new Object();
+
+animal['name'] = 'Clifford'
+animal['size'] = 'Big'
+animal['type'] = 'Dog'
+
+console.log(animal);
+console.log(animal.size);
+
+animal.color = 'red';
+
+console.log(animal);
+
+//literal notation is a quicker way of doing the above Object
+
+let vehicle = {
+    "type" : "bike",
+    "seats" : 1,
+    "wheels": 2
+}
+
+console.log(vehicle);
+
+
+let garage = [
+    vehicle, 
+    {"type": "car", "seats":5, "wheels":4}
+]
+
+console.log(garage);
+
+// to iterate oven an array 
+
+let z = [`a`, `b`, `c`, `d`];
+
+for (let i = 0; i <z.length; i++){
+    console.log(z[i]);
+}
+
+
+
+// JSON
+
+let newJSON = {
+    "name" : "will",
+    "age" : 23
+}
+//JSON to string
+
+let str = JSON.stringify(newJSON);
+console.log(str);
+
+// string to JSON
+
+let unSTR = JSON.parse(str);
+console.log(unSTR);
+
+// we want to use this to convert an object into a serialised JSON string
+
